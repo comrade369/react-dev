a2fd9773fb1106fa946639b9d1d2c1be0b358f63
comrade369 <saveallchildreninworld@gmail.com>
2023-12-02 19:08:19 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Button from "./react-dev/Button";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+    <Button />
+);
